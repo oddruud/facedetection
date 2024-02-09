2c83f542cd50d1a63f2fc6f547bfe96e61df53f5
ruudodk <ruudodk@gmail.com>
2024-02-09 22:36:53 +0100

    Add face verification, recognition, and detection functionality

--- main.py
+++ main.py
@@ -1,2 +1,45 @@
 from deepface import DeepFace
 
+backends = [
+  'opencv', 
+  'ssd', 
+  'dlib', 
+  'mtcnn', 
+  'retinaface', 
+  'mediapipe',
+  'yolov8',
+  'yunet',
+  'fastmtcnn',
+]
+
+test_img_path = "test_data/test.jpg"
+database = "test_data/database"
+
+metrics = ["cosine", "euclidean", "euclidean_l2"]
+
+#face verification
+result = DeepFace.verify(img1_path = test_img_path, 
+          img2_path = "img2.jpg", 
+          distance_metric = metrics[1]
+)
+
+print("VERIFY RESULT: ", result)
+
+#face recognition
+dfs = DeepFace.find(img_path = test_img_path, 
+          db_path = database, 
+          distance_metric = metrics[2]
+)
+
+print("FIND RESULT: ", result)
+
+#face detection and alignment
+face_objs = DeepFace.extract_faces(img_path = test_img_path, 
+        target_size = (224, 224), 
+        detector_backend = backends[4]
+)
+
+print("Face objects: ", face_objs)
+
+
+
